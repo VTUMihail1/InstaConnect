a3f3b56d638fbeafe15dc11cba559a5ae7edace8
Mihail <vtumihail@gmail.com>
2023-09-06 23:23:57 +0300

    Folder and appsettings configuration

---
+++ InstaConnect.Data/InstaConnectContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstaConnect.Data
+{
+    public class InstaConnectContext : IdentityDbContext
+    {
+        public InstaConnectContext(DbContextOptions<InstaConnectContext> options) : base(options)
+        {  }
+    }
+}
