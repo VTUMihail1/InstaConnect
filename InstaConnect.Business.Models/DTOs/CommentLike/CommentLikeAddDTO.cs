c2ffe6584883e11be73a937a08bc2a363c187fcc
Mihail <vtumihail@gmail.com>
2023-09-25 16:49:50 +0300

    Added the DTO attributes to ensure that the ReceiverId can't be the same as the SenderId.

--- InstaConnect.Business.Models/DTOs/CommentLike/CommentLikeAddDTO.cs
+++ InstaConnect.Business.Models/DTOs/CommentLike/CommentLikeAddDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace InstaConnect.Business.Models.DTOs.CommentLike
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InstaConnect.Business.Models.DTOs.CommentLike
 {
     public class CommentLikeAddDTO
     {
+        [Required]
         public string PostCommentId { get; set; }
 
+        [Required]
         public string UserId { get; set; }
     }
 }
