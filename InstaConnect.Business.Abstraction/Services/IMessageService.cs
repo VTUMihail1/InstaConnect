f5bd1a4d569444907529bdebc264ed6463006fca
Mihail <vtumihail@gmail.com>
2023-09-27 01:29:33 +0300

    Updated XML documentation for services.

--- InstaConnect.Business.Abstraction/Services/IMessageService.cs
+++ InstaConnect.Business.Abstraction/Services/IMessageService.cs
@@ -3,14 +3,66 @@ using InstaConnect.Business.Models.Results;
 
 namespace InstaConnect.Business.Abstraction.Services
 {
+    /// <summary>
+    /// Represents a service for managing messages.
+    /// </summary>
     public interface IMessageService
     {
+        /// <summary>
+        /// Retrieves all messages between a sender and a receiver.
+        /// </summary>
+        /// <param name="senderId">The ID of the sender.</param>
+        /// <param name="receiverId">The ID of the receiver.</param>
+        /// <returns>A collection of <see cref="MessageResultDTO"/> representing messages.</returns>
         Task<ICollection<MessageResultDTO>> GetAllAsync(string senderId, string receiverId);
+
+        /// <summary>
+        /// Retrieves a message by its unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the message.</param>
+        /// <returns>An <see cref="IResult{T}"/> containing a <see cref="MessageResultDTO"/>.</returns>
         Task<IResult<MessageResultDTO>> GetByIdAsync(string id);
+
+        /// <summary>
+        /// Retrieves a message by sender ID and receiver ID.
+        /// </summary>
+        /// <param name="senderId">The ID of the sender.</param>
+        /// <param name="receiverId">The ID of the receiver.</param>
+        /// <returns>An <see cref="IResult{T}"/> containing a <see cref="MessageResultDTO"/>.</returns>
         Task<IResult<MessageResultDTO>> GetBySenderIdAndReceiverIdAsync(string senderId, string receiverId);
+
+        /// <summary>
+        /// Adds a new message.
+        /// </summary>
+        /// <param name="currentUserId">The ID of the current user performing the action.</param>
+        /// <param name="messageAddDTO">The data for the new message.</param>
+        /// <returns>An <see cref="IResult{T}"/> containing the added <see cref="MessageResultDTO"/>.</returns>
         Task<IResult<MessageResultDTO>> AddAsync(string currentUserId, MessageAddDTO messageAddDTO);
+
+        /// <summary>
+        /// Updates an existing message by its unique identifier.
+        /// </summary>
+        /// <param name="currentUserId">The ID of the current user performing the action.</param>
+        /// <param name="id">The unique identifier of the message to update.</param>
+        /// <param name="messageUpdateDTO">The data to update the message with.</param>
+        /// <returns>An <see cref="IResult{T}"/> containing the updated <see cref="MessageResultDTO"/>.</returns>
         Task<IResult<MessageResultDTO>> UpdateAsync(string currentUserId, string id, MessageUpdateDTO messageUpdateDTO);
+
+        /// <summary>
+        /// Deletes all messages between a sender and a receiver.
+        /// </summary>
+        /// <param name="currentUserId">The ID of the current user performing the action.</param>
+        /// <param name="senderId">The ID of the sender.</param>
+        /// <param name="receiverId">The ID of the receiver.</param>
+        /// <returns>An <see cref="IResult{T}"/> containing the deleted <see cref="MessageResultDTO"/>.</returns>
         Task<IResult<MessageResultDTO>> DeleteBySenderIdAndReceiverIdAsync(string currentUserId, string senderId, string receiverId);
+
+        /// <summary>
+        /// Deletes a message by its unique identifier.
+        /// </summary>
+        /// <param name="currentUserId">The ID of the current user performing the action.</param>
+        /// <param name="id">The unique identifier of the message to delete.</param>
+        /// <returns>An <see cref="IResult{T}"/> containing the deleted <see cref="MessageResultDTO"/>.</returns>
         Task<IResult<MessageResultDTO>> DeleteAsync(string currentUserId, string id);
     }
 }
--- InstaConnect.Business.Abstraction/Services/IPostLikeService.cs
+++ InstaConnect.Business.Abstraction/Services/IPostLikeService.cs
@@ -3,14 +3,57 @@ using InstaConnect.Business.Models.Results;
 
 namespace InstaConnect.Business.Abstraction.Services
 {
+    /// <summary>
+    /// Represents a service for managing post likes.
+    /// </summary>
     public interface IPostLikeService
     {
+        /// <summary>
+        /// Retrieves all post likes associated with a user and a post.
+        /// </summary>
+        /// <param name="userId">The ID of the user.</param>
+        /// <param name="postId">The ID of the post.</param>
+        /// <returns>A collection of <see cref="PostLikeResultDTO"/> representing post likes.</returns>
         Task<ICollection<PostLikeResultDTO>> GetAllAsync(string userId, string postId);
+
+        /// <summary>
+        /// Retrieves a post like by its unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the post like.</param>
+        /// <returns>An <see cref="IResult{T}"/> containing a <see cref="PostLikeResultDTO"/>.</returns>
         Task<IResult<PostLikeResultDTO>> GetByIdAsync(string id);
+
+        /// <summary>
+        /// Retrieves a post like by user ID and post ID.
+        /// </summary>
+        /// <param name="userId">The ID of the user.</param>
+        /// <param name="postId">The ID of the post.</param>
+        /// <returns>An <see cref="IResult{T}"/> containing a <see cref="PostLikeResultDTO"/>.</returns>
         Task<IResult<PostLikeResultDTO>> GetByUserIdAndPostIdAsync(string userId, string postId);
+
+        /// <summary>
+        /// Adds a new post like.
+        /// </summary>
+        /// <param name="currentUserId">The ID of the current user performing the action.</param>
+        /// <param name="likeAddDTO">The data for the new post like.</param>
+        /// <returns>An <see cref="IResult{T}"/> containing the added <see cref="PostLikeResultDTO"/>.</returns>
         Task<IResult<PostLikeResultDTO>> AddAsync(string currentUserId, PostLikeAddDTO likeAddDTO);
+
+        /// <summary>
+        /// Deletes a post like by user ID and post ID.
+        /// </summary>
+        /// <param name="currentUserId">The ID of the current user performing the action.</param>
+        /// <param name="userId">The ID of the user who liked the post.</param>
+        /// <param name="postId">The ID of the post.</param>
+        /// <returns>An <see cref="IResult{T}"/> containing the deleted <see cref="PostLikeResultDTO"/>.</returns>
         Task<IResult<PostLikeResultDTO>> DeleteByUserIdAndPostIdAsync(string currentUserId, string userId, string postId);
+
+        /// <summary>
+        /// Deletes a post like by its unique identifier.
+        /// </summary>
+        /// <param name="currentUserId">The ID of the current user performing the action.</param>
+        /// <param name="id">The unique identifier of the post like to delete.</param>
+        /// <returns>An <see cref="IResult{T}"/> containing the deleted <see cref="PostLikeResultDTO"/>.</returns>
         Task<IResult<PostLikeResultDTO>> DeleteAsync(string currentUserId, string id);
     }
-
 }
