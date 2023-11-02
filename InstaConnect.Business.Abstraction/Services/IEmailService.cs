59daa8eb6da85756f0dffbd18ec2ef973dab95ef
Mihail <vtumihail@gmail.com>
2023-11-02 11:59:18 +0200

    Modified email and token services to return good written results.

---
+++ InstaConnect.Business.Abstraction/Services/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using InstaConnect.Business.Models.DTOs.Email;
+using InstaConnect.Business.Models.Results;
+
+namespace InstaConnect.Business.Abstraction.Services
+{
+    public interface IEmailService
+    {
+        Task<IResult<EmailResultDTO>> SendEmailConfirmationAsync(string email, string userId, string token);
+
+        Task<IResult<EmailResultDTO>> SendPasswordResetAsync(string email, string userId, string token);
+    }
+}
---
+++ InstaConnect.Business.Abstraction/Services/ITokenService.cs
@@ -0,0 +1,18 @@
+﻿using InstaConnect.Business.Models.DTOs.Token;
+using InstaConnect.Business.Models.Results;
+
+namespace InstaConnect.Business.Abstraction.Services
+{
+    public interface ITokenService
+    {
+        Task<IResult<TokenResultDTO>> GenerateAccessTokenAsync(string userId);
+
+        Task<IResult<TokenResultDTO>> GenerateEmailConfirmationTokenAsync(string userId);
+
+        Task<IResult<TokenResultDTO>> GeneratePasswordResetTokenAsync(string userId);
+
+        Task<IResult<TokenResultDTO>> GetByValueAsync(string value);
+
+        Task<IResult<TokenResultDTO>> DeleteAsync(string value);
+    }
+}
