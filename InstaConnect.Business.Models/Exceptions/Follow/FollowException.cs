6289414008722ebefeb47fd8d6af21da7d6ac8a0
Mihail <vtumihail@gmail.com>
2024-02-13 18:16:19 +0200

    Added custom exceptions and a global exception handler.

---
+++ InstaConnect.Business.Models/Exceptions/Follow/FollowException.cs
@@ -0,0 +1,18 @@
+﻿using InstaConnect.Business.Models.Enums;
+using InstaConnect.Business.Models.Exceptions.Base;
+
+namespace InstaConnect.Business.Models.Exceptions.Follow
+{
+    public class FollowException : BaseException
+    {
+        public FollowException(string message, InstaConnectStatusCode statusCode)
+            : base(message, statusCode)
+        {
+        }
+
+        public FollowException(string message, Exception exception, InstaConnectStatusCode statusCode)
+            : base(message, exception, statusCode)
+        {
+        }
+    }
+}
---
+++ InstaConnect.Business.Models/Exceptions/PostLike/PostNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using InstaConnect.Business.Models.Enums;
+
+namespace InstaConnect.Business.Models.Exceptions.PostLike
+{
+    public class PostLikeNotFoundException : PostLikeException
+    {
+        private const string ERROR_MESSAGE = "Post like not found";
+
+        public PostLikeNotFoundException() : base(ERROR_MESSAGE, InstaConnectStatusCode.NotFound)
+        {
+        }
+    }
+}
