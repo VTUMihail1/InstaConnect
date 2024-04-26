69d1c4c470121481e4dc743102ab976b28fba140
vtumihail <vtumihail@gmail.com>
2024-04-26 13:15:51 +0300

    Removed unused assemblies from the folder.

--- InstaConnect.Business/Models/DTOs/Token/TokenGenerateDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InstaConnect.Business.Models.DTOs.Token
-{
-    public class TokenGenerateDTO
-    {
-        public string UserId { get; set; }
-
-        public string Role { get; set; }
-    }
-}
