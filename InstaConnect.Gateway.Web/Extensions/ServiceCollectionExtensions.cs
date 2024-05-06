b69c2d673ccc4d615d925abb318e9a4fa4a745f9
vtumihail <vtumihail@gmail.com>
2024-05-06 14:35:20 +0300

    Added API gateway using YARN.

---
+++ InstaConnect.Gateway.Web/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace InstaConnect.Gateway.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddWebLayer(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection
+                .AddReverseProxy()
+                .LoadFromConfig(configuration.GetSection("ReverseProxy"));
+
+            return serviceCollection;
+        }
+    }
+}
