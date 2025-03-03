c5a36e4639bee2259cb9ad3d68d67f9908118be0
Mihail <mihail.nikolov@ventionteams.com>
2025-03-03 17:25:12 +0200

    Added interface methods for all post svc.

--- src/Services/Posts/InstaConnect.Posts.Domain/Features/PostComments/Abstractions/IPostCommentFactory.cs
+++ src/Services/Posts/InstaConnect.Posts.Domain/Features/PostComments/Abstractions/IPostCommentFactory.cs
@@ -2,7 +2,7 @@
 
 namespace InstaConnect.Posts.Domain.Features.PostComments.Abstractions;
 
-internal interface IPostCommentFactory
+public interface IPostCommentFactory
 {
     public PostComment Get(string postId, string userId, string content);
 }
--- src/Services/Posts/InstaConnect.Posts.Domain/Features/PostComments/Abstractions/IPostCommentService.cs
+++ src/Services/Posts/InstaConnect.Posts.Domain/Features/PostComments/Abstractions/IPostCommentService.cs
@@ -1,11 +1,12 @@
 ﻿using InstaConnect.Posts.Domain.Features.PostComments.Models.Entities;
 
 namespace InstaConnect.Posts.Domain.Features.PostComments.Abstractions;
-internal interface IPostCommentService
+public interface IPostCommentService
 {
     public void Update(PostComment postComment, string content);
 
     public void AddLike(PostComment postComment, string userId);
 
     public void RemoveLike(PostComment postComment, string postCommentLikeId);
+
 }
